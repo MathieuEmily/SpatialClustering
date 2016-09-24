9ac15108d8e6ccb3f399b9eee66d76c0ba3af85f
MathieuEmily <memily@MacBook-Pro-de-memily-mbp.local>
2016-09-24 23:12:45 +0200

    Version1

--- R/SpatialClustering.R
+++ R/SpatialClustering.R
@@ -1,5 +1,5 @@
 SpatialClustering <- function(data,window,Homogeneous=TRUE,Z=NULL,B=500,method.cut=c("gap","tree","none"),h=NULL,k=NULL){
-	method.cut <- match.arg(method)
+	method.cut <- match.arg(method.cut)
 	TandP <- generateListTandP(data=data,w0=window,Homogeneous=Homogeneous,Z=Z)
 	MatDist <- getMatDist(TandP$Probabilities,TandP$Trajectories)
 	clust <- getClusters(MatDist=MatDist,data=data,B=B,method=method.cut,h=h,k=k)
