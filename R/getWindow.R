d6c6c98cdb71108d5bca908f1cb3fa125cd942a9
MathieuEmily <memily@MacBook-Pro-de-memily-mbp.local>
2016-09-25 22:30:47 +0200

    Version 1

--- R/getWindow.R
+++ R/getWindow.R
@@ -3,15 +3,15 @@ getWindow <- function(data,T,w0){
 	window <- list()
 	if (T$method=="maximum"){
 		for (i in 1:(n-1)){
-			w <- owin(c(data$x[T$trajectory[i]]-T$distance[[i]],data$x[T$trajectory[i]]+T$distance[[i]]),c(data$y[T$trajectory[i]]-T$distance[[i]],data$y[T$trajectory[i]]+T$distance[[i]]))
-			w <- intersect.owin(w0,w)
+			w <- spatstat::owin(c(data$x[T$trajectory[i]]-T$distance[[i]],data$x[T$trajectory[i]]+T$distance[[i]]),c(data$y[T$trajectory[i]]-T$distance[[i]],data$y[T$trajectory[i]]+T$distance[[i]]))
+			w <- spatstat::intersect.owin(w0,w)
 			window[[i]] <- w
 			}
 		}
 	if (T$method=="euclidean"){
 		for (i in 1:(n-1)){
 			w <- spatstat::disc(radius=T$distance[i],centre=c(data$x[T$trajectory[i]],data$y[T$trajectory[i]])) ## possible conflit avec la fonction disc du package ade4
-			w <- intersect.owin(w0,w)
+			w <- spatstat::intersect.owin(w0,w)
 			window[[i]] <- w
 			}
 		}
