44cc52a83926c11a7b649b0554ba5e66de923112
MathieuEmily <memily@MacBook-Pro-de-memily-mbp.local>
2016-10-16 14:55:53 +0200

    Add other metrics

--- R/SpatialClustering.R
+++ R/SpatialClustering.R
@@ -1,6 +1,6 @@
 SpatialClustering <- function(data,window,metric="euclidean",Homogeneous=TRUE,Z=NULL,B=500,method.cut=c("gap","tree","none"),h=NULL,k=NULL){
 	method.cut <- match.arg(method.cut)
-	TandP <- generateListTandP(data=data,w0=window,distance=distance,Homogeneous=Homogeneous,Z=Z)
+	TandP <- generateListTandP(data=data,w0=window,metric=metric,Homogeneous=Homogeneous,Z=Z)
 	MatDist <- getMatDist(TandP$Probabilities,TandP$Trajectories)
 	clust <- getClusters(MatDist=MatDist,data=data,B=B,method=method.cut,h=h,k=k)
 	res <- list(data=data,window=window,TandP=TandP,MatDist=MatDist,hh=clust$hh,group=clust$group,ngroup=clust$ngroup,Homogeneous=Homogeneous,Z=Z,metric=metric)
